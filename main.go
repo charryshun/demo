d3695ccb439ec6eb3f4ee8f7aa4e03dd89117741
ywysuibian <ywysuibian@163.com>
2021-02-03 20:23:51 +0800

    Update main.go

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/{name}", func(w http.ResponseWriter, r *http.Request) {
 		vars := mux.Vars(r)
-		fmt.Fprintf(w, "Welcome {%s}!\n%s", vars["name"], time.Now())
+		fmt.Fprintf(w, "Welcome {%s}!\n%s", vars["name"], time.Now()。Format("2006-01-02 15:04:05"))
 	})
 	err := http.ListenAndServe(":8080", r)
 	if err != nil {
