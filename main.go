784ed6d8210654c5fde86c90c774985155217c85
Wen Yan Yang <wyyangbj@cn.ibm.com>
2021-02-04 15:30:18 +0800

    chart

--- main.go
+++ main.go
@@ -10,6 +10,9 @@ import (
 
 func main() {
 	r := mux.NewRouter()
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Greeting: Welcome!\n%s", time.Now().Format("2006-01-02 15:04:05"))
+	})
 	r.HandleFunc("/{name}", func(w http.ResponseWriter, r *http.Request) {
 		vars := mux.Vars(r)
 		fmt.Fprintf(w, "Greeting: Welcome {%s}!\n%s", vars["name"], time.Now().Format("2006-01-02 15:04:05"))
